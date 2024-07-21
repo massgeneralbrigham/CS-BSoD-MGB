c51f41e00ba53feaf318c40a6a8d5bb3c51fa9da
Kapasakalidis, Panagiotis <pkapasakalidis@mgb.org>
2024-07-21 12:23:56 -0400

    Update CSRecoverSample.ps

--- CSRecoverSample.ps1
+++ CSRecoverSample.ps1
@@ -1,27 +1,43 @@
 <#
 .SYNOPSIS
-  This script remidates a device impacted by the CrowdStrike update.
+  This script remediates a device impacted by the CrowdStrike update.
 
 .DESCRIPTION
-  This script uses are HashTable to match the recovery ID to get the key.
+  During boot, this script obtains the ID from impacted device using
+  manage-bde, scrapes the ID, and matches it against your included
+  Hash Table for the key.  The key is used to unlock the device and
+  delete the crowdstrike c-00000291*.sys file.
+
+  It is recommended that the script be compiled into a executable so
+  the Hash Table isn't exposed to anyone that can read the iso contents.
+  Other security controls may be warranted and should be reviewed by 
+  your security team.
+
+  IDs and Key can be extracted from your MBAM database using the following
+  method:
+  Open the SQL Management Studio, and Expand the MBAM_Recovery_and_Hardware database.
+  Under Tables, Select RecoveryAndHardwareCore.Keys, and use the following SQLquery:
+  Select [RecoveryKeyID], [RecoveryKey] FROM [MBAM_Recovery_and_Hardware].[RecoveryAndHardwareCore].[Keys]
+  
+  Export the results to csv or txt and format it into a Hash Table format "PUTIDHERE" = "PUTKEYHERE".
 
 .NOTES
-  Version:        1.1
-  Author:         Mass General Brigham 
-  Creation Date:  7/20/2024
+  Version:        1.2
+  Author:         MGB
+  Creation Date:  7/21/2024
 #>
 
-# Recovery Keys HashTable >>> this is where you will find a way to extract your BitLocker keys and create this hash table. The way to extract keys is specific to your organization. So ask your Windows admninstrator for access, and create the hash table with IDs and KEYS
-
+# Recovery Keys HashTable
 $recoveryKeys = @{
     "PUTIDHERE" = "PUTKEYHERE"
     "PUTIDHERE" = "PUTKEYHERE"
     "PUTIDHERE" = "PUTKEYHERE"
     "PUTIDHERE" = "PUTKEYHERE"
-    "PUTIDHERE" = "PUTKEYHERE"
-    "PUTIDHERE" = "PUTKEYHERE"
-    "PUTIDHERE" = "PUTKEYHERE"
     }
+    
+    #Set Variables
+    $recoveryKeyID = ""
+    $matchingRecoveryKey =""
     
     # Get Recovery ID from Local Machine
     try
@@ -34,7 +50,9 @@ $recoveryKeys = @{
         Write-host -f red "Encountered Error:"$_.Exception.Message
     }
     # Search for the Recovery Key based on Recovery ID
-    $matchingRecoveryKey = $recoveryKeys.$recoveryKeyID.ToString()
+    if ($recoveryKeys.ContainsKey($recoveryKeyID)) {
+        $matchingRecoveryKey = $recoveryKeys.$recoveryKeyID.ToString()
+    }
     
     # Check if a matching Recovery Key was found, unlock drive if found, and delete CS files
     if ($matchingRecoveryKey.length -gt 0)
